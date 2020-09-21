a28270517e75771b1ca949bc8c5dc1e70d3e54e5
Hubert Liberacki <hliberacki@gmail.com>
2020-09-21 14:36:09 +0200

    Change columns limit from 120 to 100

--- tests/test_runner/main.cpp
+++ tests/test_runner/main.cpp
@@ -5,24 +5,25 @@
 
 int main(int argc, char* argv[])
 {
-    trompeloeil::set_reporter([](trompeloeil::severity s, const char* file, uint64_t line, std::string const& msg) {
-        std::ostringstream os;
-        if (line)
-        {
-            os << file << ':' << line << '\n';
-        }
-        os << msg;
-        auto failure = os.str();
-        if (s == trompeloeil::severity::fatal)
-        {
-            FAIL(failure);
-        }
-        else
-        {
-            CAPTURE(failure);
-            CHECK(failure.empty());
-        }
-    });
+    trompeloeil::set_reporter(
+      [](trompeloeil::severity s, const char* file, uint64_t line, std::string const& msg) {
+          std::ostringstream os;
+          if (line)
+          {
+              os << file << ':' << line << '\n';
+          }
+          os << msg;
+          auto failure = os.str();
+          if (s == trompeloeil::severity::fatal)
+          {
+              FAIL(failure);
+          }
+          else
+          {
+              CAPTURE(failure);
+              CHECK(failure.empty());
+          }
+      });
 
     return Catch::Session().run(argc, argv);
 }
